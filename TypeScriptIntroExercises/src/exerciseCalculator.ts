336bd711605cfdda99ad29a5736b3e3c84af165a
Jacob <jbale444@outlook.com>
2024-01-23 10:16:02 +0000

    Setup typeScript development environment and created two simple calculators using typeScript for BMI and exercises which can be run via the command line for exercises 9.1 to 9.3

---
+++ TypeScriptIntroExercises/src/exerciseCalculator.ts
@@ -0,0 +1,43 @@
+interface Training {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number
+};
+
+const calculateExercises = (daily: number[], target: number): Training => {
+    const daysTrained = daily.length;
+    const trainingTotal = daily.reduce((total, current) => total + current);
+    console.log("total", trainingTotal)
+    const average = trainingTotal / daysTrained;
+    const percentageOfTarget = (average / target) * 100;
+
+    let rating = 3;
+    let ratingDescription = 'not too bad but could be better';
+    if (percentageOfTarget >= 100) {
+        rating = 1;
+        ratingDescription = 'Amazing! Keep Going!';
+    } else if ( percentageOfTarget >= 50) {
+        rating = 2;
+        ratingDescription = 'Making Progress! Keep pushing!';
+    }
+
+    return {
+        periodLength: daysTrained,
+        trainingDays: daily.reduce((trained, current) => current == 0 ? trained : trained + 1),
+        success: average > target ? true : false,
+        rating: rating,
+        ratingDescription: ratingDescription,
+        target: target,
+        average: average
+    };
+};
+
+
+const target = parseInt(process.argv[2]);
+const daily = process.argv.slice(3).map(hours => +hours);
+
+console.log(calculateExercises(daily, target));
